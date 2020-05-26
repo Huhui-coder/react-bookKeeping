fdd84ba479c14423a305c9f13c246bd72c60a1f0
huhui <569263082@qq.com>
2020-05-26 17:13:03 +0800

    feat: init

---
+++ src/app.jsx
@@ -0,0 +1,77 @@
+import Taro, { Component } from '@tarojs/taro'
+import { Provider } from '@tarojs/mobx'
+import Index from './pages/index'
+
+import counterStore from './store/counter'
+
+import './app.less'
+import 'taro-ui/dist/style/index.scss'
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+const store = {
+  counterStore
+}
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/index/index',
+      'pages/cate/index',
+      'pages/person/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'book-keeping',
+      navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      color: "#666",
+      selectedColor: "#b4282d",
+      backgroundColor: "#fafafa",
+      borderStyle: 'black',
+      list: [{
+        pagePath: "pages/index/index",
+        iconPath: "./assets/tab-bar/home.png",
+        selectedIconPath: "./assets/tab-bar/home-active.png",
+        text: "首页"
+      }, {
+        pagePath: "pages/cate/index",
+        iconPath: "./assets/tab-bar/cate.png",
+        selectedIconPath: "./assets/tab-bar/cate-active.png",
+        text: "分类"
+      }, {
+        pagePath: "pages/person/index",
+        iconPath: "./assets/tab-bar/user.png",
+        selectedIconPath: "./assets/tab-bar/user-active.png",
+        text: "个人"
+      }]
+    }
+  }
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render () {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
---
+++ src/pages/cate/index.jsx
@@ -0,0 +1,33 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { observer, inject } from '@tarojs/mobx'
+import './index.less'
+
+
+@inject('counterStore')
+@observer
+class Person extends Component {
+
+  componentWillMount() { }
+
+  componentWillReact() { }
+
+  componentDidMount() { }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+ 
+  render() {
+    const { counterStore: { counter } } = this.props
+    return (
+      <View className='index'>
+     cate
+      </View>
+    )
+  }
+}
+
+export default Person 
